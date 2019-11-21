2caaa2f8055eca0a56f98bae1c9b4e09d674a84a
johnvelis <john@johnvelis.com>
2019-11-21 09:10:32 -0500

    Initial Push

--- Assessment_ClientApp2/Monster.cs
+++ Assessment_ClientApp2/Monster.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Monsters
+namespace Assessment_ClientApp2
 {
     public class Monster
     {
--- Assessment_ClientApp2/Program.cs
+++ Assessment_ClientApp2/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace Monsters
+namespace Assessment_ClientApp2
 {
     // **************************************************
     //
